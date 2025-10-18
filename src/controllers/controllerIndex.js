7dc49b9baf76462d29d07a358afd36919895fc04
Guilherme Marques <81587983+guinorbona@users.noreply.github.com>
2025-10-18 16:43:44 -0300

    Estruturação da Base
    Estruturação do MVC, criação das Routes, Views, Models, Controllers e conexão com o Banco de Dados.

---
+++ src/controllers/controllerIndex.js
@@ -0,0 +1,6 @@
+// const db = require('../db');
+const { model } = require('../models/modelGaleria');
+
+module.exports.index = (app, req, res) => {
+        res.render('index');
+};
---
+++ src/controllers/controllerMaisDetalhes.js
@@ -0,0 +1,6 @@
+// const db = require('../db');
+const { model } = require('../models/modelGaleria');
+
+module.exports.maisDetalhes = (app, req, res) => {
+        res.render('maisDetalhes');
+};
